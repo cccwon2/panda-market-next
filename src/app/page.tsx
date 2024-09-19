b918dad395de7a0f21d2d133ed3162cec64509d8
cccwon2 <cccwon2@naver.com>
2024-09-19 18:25:48 +0900

    chore: 홈페이지 css 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,17 +13,17 @@ const HomePage: React.FC = () => {
   return (
     <>
       <section
-        className="bg-blue-500 h-[60vh] text-center bg-no-repeat bg-bottom bg-[length:130%] md:h-[90vh] md:bg-[length:120%] lg:text-left lg:h-[540px] lg:flex lg:flex-col lg:justify-center lg:bg-[80%_bottom] lg:bg-[length:55%]"
+        className="bg-blue-100 w-full h-[60vh] min-h-[500px] text-center bg-no-repeat bg-bottom bg-[length:130%] md:h-[90vh] md:bg-[length:120%] lg:text-left lg:h-[540px] lg:flex lg:flex-col lg:justify-center lg:bg-[80%_bottom] lg:bg-[length:55%]"
         style={{ backgroundImage: `url(${heroImage.src})` }}
       >
-        <div className="lg:max-w-[1200px] lg:w-full lg:mx-auto">
-          <h1 className="font-bold text-3xl leading-[44.8px] pt-12 md:text-4xl md:leading-[56px] md:pt-[84px] md:pb-6 lg:pt-0 lg:pb-8">
+        <div className="min-w-[400px] max-w-[1200px] w-full mx-auto px-4 pt-[30px]">
+          <div className="font-bold sm:pt-[100px] min-w-[300px] text-lg leading-tight pt-12 md:text-4xl md:leading-tight md:pt-[84px] md:pb-6 lg:pt-0 lg:pb-8">
             일상의 모든 물건을 <br className="md:hidden lg:inline" />
             거래해 보세요
-          </h1>
+          </div>
           <Link
             href="/items"
-            className="inline-block px-6 py-3 bg-blue-500 text-white rounded-full font-medium"
+            className="inline-block mt-[30px] px-8 py-4 w-[20vw] min-w-[200px] max-w-[340px] bg-blue-500 text-white rounded-full font-bold text-lg hover:bg-blue-600 transition-colors duration-300"
           >
             구경하러 가기
           </Link>
@@ -58,10 +58,10 @@ const HomePage: React.FC = () => {
       </section>
 
       <section
-        className="bg-blue-500 h-[60vh] text-center bg-no-repeat bg-bottom bg-[length:130%] md:h-[90vh] md:bg-[length:120%] lg:text-left lg:h-[540px] lg:flex lg:flex-col lg:justify-center lg:bg-[80%_bottom] lg:bg-[length:55%]"
+        className="bg-blue-100 w-full h-[60vh] text-center bg-no-repeat bg-bottom bg-[length:130%] md:h-[90vh] md:bg-[length:120%] lg:text-left lg:h-[540px] lg:flex lg:flex-col lg:justify-center lg:bg-[80%_bottom] lg:bg-[length:55%]"
         style={{ backgroundImage: `url(${bottomBannerImage.src})` }}
       >
-        <div className="lg:max-w-[1200px] lg:w-full lg:mx-auto">
+        <div className="max-w-[1200px] w-full mx-auto px-4">
           <h1 className="font-bold text-3xl leading-[44.8px] pt-12 md:text-4xl md:leading-[56px] md:pt-[84px] md:pb-6 lg:pt-0 lg:pb-8">
             믿을 수 있는
             <br />
